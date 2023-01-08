55aabfc166f774fc207acd0ed9984b1155d8039a
Luis Sanchez <dceluis@outlook.com>
2023-01-08 14:16:15 -0500

    Update deps.ps1

--- deps.ps1
+++ deps.ps1
@@ -26,34 +26,34 @@ Install-Module PSWindowsUpdate -Scope CurrentUser -Force
 
 
 # system and cli
-winget install Microsoft.WebPICmd                        --silent --accept-package-agreements
+#winget install Microsoft.WebPICmd                        --silent --accept-package-agreements
 winget install Git.Git                                   --silent --accept-package-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
 winget install OpenJS.NodeJS                             --silent --accept-package-agreements
-winget install Python.Python.3                           --silent --accept-package-agreements
-winget install RubyInstallerTeam.Ruby                    --silent --accept-package-agreements
+#winget install Python.Python.3                           --silent --accept-package-agreements
+#winget install RubyInstallerTeam.Ruby                    --silent --accept-package-agreements
 
 # browsers
 winget install Google.Chrome                             --silent --accept-package-agreements
 winget install Mozilla.Firefox                           --silent --accept-package-agreements
-winget install Opera.Opera                               --silent --accept-package-agreements
+#winget install Opera.Opera                               --silent --accept-package-agreements
 
 # dev tools and frameworks
 winget install Microsoft.PowerShell                      --silent --accept-package-agreements
-winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements
-winget install Microsoft.SQLServerManagementStudio       --silent --accept-package-agreements
-winget install Microsoft.VisualStudio.2022.Professional  --silent --accept-package-agreements --override "--wait --quiet --norestart --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
-winget install JetBrains.dotUltimate                     --silent --accept-package-agreements --override "/SpecificProductNames=ReSharper;dotTrace;dotCover /Silent=True /VsVersion=17.0"
+#winget install Microsoft.SQLServer.2019.Developer        --silent --accept-package-agreements
+#winget install Microsoft.SQLServerManagementStudio       --silent --accept-package-agreements
+#winget install Microsoft.VisualStudio.2022.Professional  --silent --accept-package-agreements --override "--wait --quiet --norestart --nocache --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
+#winget install JetBrains.dotUltimate                     --silent --accept-package-agreements --override "/SpecificProductNames=ReSharper;dotTrace;dotCover /Silent=True /VsVersion=17.0"
 winget install Vim.Vim                                   --silent --accept-package-agreements
-winget install WinMerge.WinMerge                         --silent --accept-package-agreements
-winget install Microsoft.AzureCLI                        --silent --accept-package-agreements
-winget install Microsoft.AzureStorageExplorer            --silent --accept-package-agreements
-winget install Microsoft.AzureStorageEmulator            --silent --accept-package-agreements
+#winget install WinMerge.WinMerge                         --silent --accept-package-agreements
+#winget install Microsoft.AzureCLI                        --silent --accept-package-agreements
+#winget install Microsoft.AzureStorageExplorer            --silent --accept-package-agreements
+#winget install Microsoft.AzureStorageEmulator            --silent --accept-package-agreements
 #winget install Microsoft.ServiceFabricRuntime            --silent --accept-package-agreements
 #winget install Microsoft.ServiceFabricExplorer           --silent --accept-package-agreements
 
 Refresh-Environment
 
-gem pristine --all --env-shebang
+#gem pristine --all --env-shebang
 
 ### Node Packages
 Write-Host "Installing Node Packages..." -ForegroundColor "Yellow"
@@ -63,11 +63,11 @@ if (which npm) {
 }
 
 ### Janus for vim
-Write-Host "Installing Janus..." -ForegroundColor "Yellow"
-if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
-    curl.exe -L https://bit.ly/janus-bootstrap | bash
-
-    cd ~/.vim/
-    git submodule update --remote
-}
+#Write-Host "Installing Janus..." -ForegroundColor "Yellow"
+#if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
+#    curl.exe -L https://bit.ly/janus-bootstrap | bash
+#
+#    cd ~/.vim/
+#    git submodule update --remote
+#}
 
